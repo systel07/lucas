ad427a2d8aac23a88114ae09bd58062fcd6841e8
systel07 <47749765+systel07@users.noreply.github.com>
2019-03-05 15:18:33 +0000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,10 +3,10 @@ ChatterBot is a machine learning, conversational dialog engine.
 """
 from .chatterbot import ChatBot
 
-__version__ = '1.0.2'
-__author__ = 'Gunther Cox'
-__email__ = 'gunthercx@gmail.com'
-__url__ = 'https://github.com/gunthercox/ChatterBot'
+__version__ = '1.0.4'
+__author__ = 'Lucas'
+__email__ = 'sytel.info7@gmail.com'
+__url__ = 'https://github.com/systel07/ChatterBot'
 
 __all__ = (
     'ChatBot',
